889d5d230bed93318d8f08c66a6628dba3726f49
Andre Barroso <andrefb.eng1@gmail.com>
2023-11-14 07:21:34 -0300

    feat: criacao de rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import {NavigationContainer, useTheme } from "@react-navigation/native"
+import { View } from "react-native"
+import { AppRoutes } from "./app.routes"
+
+export const Routes = () => {
+    
+    return (
+        <View style={{flex: 1}}>
+            <NavigationContainer >
+                <AppRoutes />
+            </NavigationContainer>
+        </View>
+    )
+}
